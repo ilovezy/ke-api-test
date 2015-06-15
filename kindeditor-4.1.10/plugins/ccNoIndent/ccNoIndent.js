c5385a2c8b51055bf524fa4f5efcad4d703538d3
misCC <1518265554@qq.com>
2015-06-15 15:49:10 +0800

    update

--- kindeditor-4.1.10/plugins/ccNoIndent/ccNoIndent.js
+++ kindeditor-4.1.10/plugins/ccNoIndent/ccNoIndent.js
@@ -4,10 +4,14 @@ KindEditor.plugin('ccNoIndent', function(K){
 
     editor.clickToolbar(name, function(){
         console.log(editor);
-        var selectedHtml = editor.selectedHtml()
         K('#showText').html('editor.text() = ' + editor.text());
         K('#showHtml').html('editor.html() = ' + editor.html());
+
+        var selectedHtml = editor.selectedHtml()
         K('#showSelectedHtml').html('editor.selectedHtml() = ' + selectedHtml);
+        console.log(selectedHtml)
+        console.log(K.unescape(selectedHtml))
+
         console.log(editor.count()) // 编辑框里的字符数量，注意是源代码的字符数量
         console.log(editor.isEmpty()) // 编辑器是否为空，其实就是 editor.count() === 0 ？
         editor.insertHtml('<div>insertHtml</div>')
